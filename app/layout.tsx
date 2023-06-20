65c1690f527f225860d05fcc2ed9d0be40849ca3
wen <wenzaiquan1995@gmail.com>
2023-06-20 23:44:41 +0800

    feature: login page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import "@/styles/globals.css"
-import { Metadata } from "next"
+import {Metadata} from "next"
 
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
+import {siteConfig} from "@/config/site"
+import {fontSans} from "@/lib/fonts"
+import {cn} from "@/lib/utils"
 
 export const metadata: Metadata = {
   title: {
@@ -15,8 +12,8 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    {media: "(prefers-color-scheme: light)", color: "white"},
+    {media: "(prefers-color-scheme: dark)", color: "black"},
   ],
   icons: {
     icon: "/favicon.ico",
@@ -29,25 +26,19 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({children}: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
+      <head/>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
       </html>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,20 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+"use client"
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
 
 export default function IndexPage() {
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+    <div className="flex h-[100vh] w-[100vw] flex-col items-center justify-center bg-[#afafaf]">
+      <div
+        className='flex h-[50vh] min-h-[500px] w-[60vw] min-w-[650px] flex-col items-center justify-center rounded-[30px] bg-[white] shadow-2xl'>
+        <div className='mb-[5vh] text-[40px] font-bold text-[#afafaf]'>
+          Welcome to the Gopher AI!
+        </div>
+        <Avatar className='h-[100px] w-[100px]'>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+          <AvatarFallback>^_^</AvatarFallback>
+        </Avatar>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    </div>
   )
 }
